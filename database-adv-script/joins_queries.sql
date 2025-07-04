1d3627e9b59cdac784d2a4217375c7ca104e2048
yussufrakkabi <yussufrakkabi@outlook.com>
2025-07-04 20:21:14 +0100

    joinssql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,72 +1,38 @@
-
--- 1. INNER JOIN: Retrieve all bookings and the respective users who made those bookings
+--INNER JOIN - Retrieve all bookings and the respective users who made those bookings.
 SELECT 
-    bookings.id AS booking_id,
-    bookings.property_id,
-    bookings.check_in,
-    bookings.check_out,
-    bookings.status,
-    users.id AS user_id,
-    users.first_name,
-    users.last_name,
-    users.email
-FROM bookings
-INNER JOIN users ON bookings.user_id = users.id;
+    users.id AS user_id, 
+    users.name AS user_name, 
+    bookings.id AS booking_id, 
+    bookings.date AS booking_date
+FROM 
+    users
+INNER JOIN 
+    bookings 
+ON 
+    users.id = bookings.user_id;
 
--- 2. LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
+--LEFT JOIN - Retrieve all properties and their reviews, including properties with no reviews.
 SELECT 
-    properties.id AS property_id,
-    properties.title,
-    properties.location,
-    reviews.id AS review_id,
-    reviews.rating,
-    reviews.comment
-FROM properties
-LEFT JOIN reviews ON properties.id = reviews.property_id;
+    properties.id AS property_id, 
+    properties.name AS property_name, 
+    reviews.id AS review_id, 
+    reviews.rating AS review_rating
+FROM 
+    properties
+LEFT JOIN 
+    reviews 
+ON 
+    properties.id = reviews.property_id;
 
--- 3. FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
--- This version is compatible with PostgreSQL
+--FULL OUTER JOIN - Retrieve all users and all bookings, even if a user has no booking or a booking is not linked to a user.
 SELECT 
-    users.id AS user_id,
-    users.first_name,
-    users.last_name,
-    bookings.id AS booking_id,
-    bookings.property_id,
-    bookings.check_in,
-    bookings.check_out
-FROM users
-FULL OUTER JOIN bookings ON users.id = bookings.user_id;
-
--- If using MySQL (which does not support FULL OUTER JOIN), use this alternative with UNION
-SELECT 
-    users.id AS user_id,
-    users.first_name,
-    users.last_name,
-    bookings.id AS booking_id,
-    bookings.property_id,
-    bookings.check_in,
-    bookings.check_out
-FROM users
-LEFT JOIN bookings ON users.id = bookings.user_id
-
-UNION
-
-SELECT 
-    users.id AS user_id,
-    users.first_name,
-    users.last_name,
-    bookings.id AS booking_id,
-    bookings.property_id,
-    bookings.check_in,
-    bookings.check_out
-FROM bookings
-LEFT JOIN users ON users.id = bookings.user_id;
-
-SELECT 
-    properties.id AS property_id,
-    properties.title,
-    reviews.id AS review_id,
-    reviews.rating,
-    reviews.comment
-FROM properties
-LEFT JOIN reviews ON properties.id = reviews.property_id;
+    users.id AS user_id, 
+    users.name AS user_name, 
+    bookings.id AS booking_id, 
+    bookings.date AS booking_date
+FROM 
+    users
+FULL OUTER JOIN 
+    bookings 
+ON 
+    users.id = bookings.user_id;
