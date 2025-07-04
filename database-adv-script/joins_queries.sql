436c52f447257555af65ad3d6da54efacba1ca23
yussufrakkabi <yussufrakkabi@outlook.com>
2025-07-04 19:50:12 +0100

    joins..

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -9,3 +9,53 @@ SELECT
     users.email
 FROM bookings
 INNER JOIN users ON bookings.user_id = users.id;
+
+SELECT 
+    properties.id AS property_id,
+    properties.title,
+    properties.location,
+    reviews.id AS review_id,
+    reviews.rating,
+    reviews.comment
+FROM properties
+LEFT JOIN reviews ON properties.id = reviews.property_id;
+
+SELECT 
+    users.id AS user_id,
+    users.first_name,
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.check_in,
+    bookings.check_out
+FROM users
+FULL OUTER JOIN bookings ON users.id = bookings.user_id;
+
+SELECT 
+    users.id AS user_id,
+    users.first_name,
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.check_in,
+    bookings.check_out
+FROM users
+FULL OUTER JOIN bookings ON users.id = bookings.user_id;
+
+SELECT 
+    users.id AS user_id,
+    users.first_name,
+    bookings.id AS booking_id,
+    bookings.property_id
+FROM users
+LEFT JOIN bookings ON users.id = bookings.user_id
+
+UNION
+
+SELECT 
+    users.id AS user_id,
+    users.first_name,
+    bookings.id AS booking_id,
+    bookings.property_id
+FROM bookings
+LEFT JOIN users ON bookings.user_id = users.id;
+
+
